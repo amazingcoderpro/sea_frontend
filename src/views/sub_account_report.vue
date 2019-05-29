ac894f2fe1f6559ed8c78b7494c1119e94e6833c
leslieyang666 <leslie951109@163.com>
2019-05-29 19:51:41 +0800

    sub_account_report.vue增加了echarts图表

--- src/views/sub_account_report.vue
+++ src/views/sub_account_report.vue
@@ -32,9 +32,9 @@
             <!-- Filter -->
           <div class="Filter">
             <el-form-item label="Filter">
-                  <el-select v-model="week" placeholder="Yesterday" class="Filter_week">
+                  <el-select v-model="Account" placeholder="Yesterday" class="Filter_week">
                       <el-option
-                      v-for="item in weekArray"
+                      v-for="item in formAccount"
                       :key="item.value"
                       :label="item.label"
                       :value="item.value">
@@ -64,10 +64,11 @@
                 </div>
             </el-form-item>
           </div>
-      </el-form>   
+        </el-form>  
+
           <!-- echarts -->
-          <div :class="className" :id="id" :style="{height:height,width:width}" ref="myEchart">
-        </div>
+          <div :class="className" :id="id" :style="{height:height,width:width}" ref="myEchart"></div> 
+  
   </div>
 </template>
 
@@ -85,15 +86,17 @@ export default {
     },
     width: {
       type: String,
-      default: '1500px'
+      default: '1600px'
     },
     height: {
       type: String,
-      default: '500px'
+      default: '400px'
     }
   },
   data() {
     return {
+      Account:"",
+      formAccount:[],
       chart: null,
       orgOptions: {},
       search:"",
@@ -135,15 +138,30 @@ export default {
             xAxis: {
             type: 'category',
             boundaryGap: false,
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            data: ['20', '21', '22', '23', '24', '25', '26','27','28','29','30','31','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','']
         },
+        grid:{               // 铺满div 
+              top:"50px",
+              left:"50px",
+              right:"15px",
+              bottom:"50px"
+        },
+        tooltip: {
+                    trigger: 'axis'  //鼠标滑过时显示数据
+                },
         yAxis: {
-            type: 'value'
+            type: 'value',
+              max: 3000,   //纵坐标的数据  最大最小值  如果不给 系统会默认分配值 会跟着值的变化而变化
+              min: 0,
         },
         series: [{
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            data: [2300, 2400, 2200, 2150, 2150, 2160, 2260,2280,2310,2360,2300,2450,2520,2330,2460,2560,2610,2530,2430,2230,2680,2610,2400,2630,2540,2960,2650,2350,2450,2330,],
             type: 'line',
-            areaStyle: {}
+            areaStyle: {},
+            color:['#0065a3'], //设置折线图内容颜色
+            smooth:true,//折点是圆弧状的
+            symbolSize: 12,   //设定实心点的大小
+         
         }]
       })
     }
